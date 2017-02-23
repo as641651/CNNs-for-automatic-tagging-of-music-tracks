2715d1f8ab8c00f7dc51bbe7332ba95689111eea
Aravind Sankaran <aravind.sankaran@rwth-aachen.de>
2017-02-23 13:53:47 +0100

    added archs with elu activation

---
+++ torch-modules/rnns/models/lstm_model2.lua
@@ -0,0 +1,29 @@
+require 'nn'
+require 'rnn'
+
+local net = {}
+
+function net.get_rnn(opt)
+
+  local rnn = nn.Sequential()
+
+  for i = 1, opt.rnn_layers do
+    local input_dim = opt.rnn_hidden_size
+    if i == 1 then
+      input_dim = opt.input_encoding_size
+    end
+    rnn:add(nn.LSTM(input_dim, opt.rnn_hidden_size))
+    local elu = nn.ELU()
+    elu.inplace = true
+    rnn:add(elu)
+    if opt.dropout > 0 then
+      rnn:add(nn.Dropout(opt.dropout))
+    end
+  end
+
+  return rnn
+
+end
+
+return net
+
